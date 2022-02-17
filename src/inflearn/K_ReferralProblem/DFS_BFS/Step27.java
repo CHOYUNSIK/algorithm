151e4325384c30d3460918a08eb2c7b47b504a2b
데이빗 <appie6069@tw.ac.kr>
2022-02-17 10:44:03 +0900

    추가

--- src/inflearn/K_ReferralProblem/DFS_BFS/Step27.java
+++ src/inflearn/K_ReferralProblem/DFS_BFS/Step27.java
@@ -9,79 +9,62 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Step27 {
-    static int w, h, result;
+    static int w, h, result, dirtyCheckNum;
     static char[][] map;
     static ArrayList<Node> list;
-    static int[][] dis;
+    static int[][] distance;
+    static boolean[] check;
 
-    static boolean[] selected;
+    static int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 
-    static void dfs(int depth, int dist, int from) {
-        if (depth == selected.length - 1) {
-            result = Math.min(result, dist);
-            return;
+    static void dfs(int start, int count, int num) {
+
+        if (count == list.size()) {
+            result = Math.min(result, num);
         }
-        for (int i = 1; i < selected.length; i++) {
 
-            if(!selected[i]){
-                selected[i] = true;
-                dfs(depth + 1, dist + dis[from][i], i );
-                selected[i] = false;
+        for (int i = 1; i < list.size(); i++) {
+            if(!check[i]){
+                check[i] = true;
+                dfs(i,count+1,num + distance[start][i]);
+                check[i] = false;
             }
-
         }
+
     }
 
-    static int bfs(int from) {
-        int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        Queue<Node> q = new LinkedList<>();
+    static void bfs(Node node, int start) {
         boolean[][] visited = new boolean[h][w];
-
-        int cleaningNum = 0;
-        int count = 0;
-
-        Node start = list.get(from);
-        visited[start.x][start.y] = true;
-        q.offer(start);
+        Queue<Node> q = new LinkedList<>();
+        q.offer(node);
+        visited[node.x][node.y] = true;
 
         while (!q.isEmpty()) {
-            count++;
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                Node cur = q.poll();
+            Node cur = q.poll();
 
-                for (int j = 0; j < 4; j++) {
-                    int nx = cur.x + move[j][0];
-                    int ny = cur.y + move[j][1];
+            if (map[cur.x][cur.y] == '*') {
+                if (start == 0) dirtyCheckNum++;
 
-                    if (nx < 0 || ny < 0 || nx >= h || ny >= w || visited[nx][ny] || map[nx][ny] == 'x') continue;
-
-                    if (map[nx][ny] == 'o' || map[nx][ny] == '*') {
-                        for (int to = 0; to < list.size(); to++) {
-                            Node end = list.get(to);
-
-                            if (nx == end.x && ny == end.y) {
-                                dis[from][to] = count;
-                                dis[to][from] = count;
-
-                                cleaningNum++;
-                            }
-
-                        }
+                for (int i = 1; i < list.size(); i++) {
+                    if (cur.x == list.get(i).x && cur.y == list.get(i).y) {
+                        distance[start][i] = cur.n;
                     }
-
-                    visited[nx][ny] = true;
-                    q.offer(new Node(nx, ny));
                 }
-
             }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + move[i][0];
+                int ny = cur.y + move[i][1];
+
+                if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+                if (visited[nx][ny] || map[nx][ny] == 'x') continue;
+
+                q.offer(new Node(nx, ny, cur.n + 1));
+                visited[nx][ny] = true;
+            }
+
         }
-
-
-        if (cleaningNum != list.size() - 1) {
-            return -1;
-        } else return 0;
 
 
     }
@@ -90,6 +73,8 @@ public class Step27 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
 
         while (true) {
 
@@ -107,47 +92,56 @@ public class Step27 {
                 map[i] = br.readLine().toCharArray();
                 for (int j = 0; j < w; j++) {
                     if (map[i][j] == 'o') {
-                        list.add(0, new Node(i, j));
+                        list.add(0, new Node(i, j, 0));
                     }
                     if (map[i][j] == '*') {
-                        list.add(new Node(i, j));
+                        list.add(new Node(i, j, 0));
                     }
                 }
             }
 
             int size = list.size();
-            dis = new int[size][size];
-            selected = new boolean[size];
+            distance = new int[size][size];
+            check = new boolean[size];
 
+            dirtyCheckNum = 0;
             for (int i = 0; i < size; i++) {
-                if (bfs(i) == -1) {
-                    result = -1;
-                    break;
-                }
+                bfs(list.get(i), i);
             }
 
-            if (result == -1) {
-                System.out.println(result);
-                continue;
-            } else {
+            if (dirtyCheckNum == list.size() - 1) {
                 result = Integer.MAX_VALUE;
-                selected[0] = true;
-                dfs(0, 0, 0);
-                System.out.println(result);
+                check = new boolean[size];
+                check[0] = true;
+                dfs(0, 1, 0);
+                sb.append(result).append("\n");
+            } else {
+                sb.append(-1).append("\n");
             }
+
+          /*  for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print(i + " 에서 ");
+                    System.out.println(j + "까지 거리 " + distance[i][j] + " ");
+                }
+                System.out.println();
+            }*/
+
 
         }
-
+        System.out.println(sb);
 
     }
 
     static class Node {
         int x;
         int y;
+        int n;
 
-        public Node(int x, int y) {
+        public Node(int x, int y, int n) {
             this.x = x;
             this.y = y;
+            this.n = n;
         }
     }
 }
