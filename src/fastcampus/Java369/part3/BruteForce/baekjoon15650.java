5ea414627a2f3042a2c0b03b9fe98820e1940dd4
CHOYUNSIK <appie6069@naver.com>
2022-05-17 11:02:50 +0900

    업로드

---
+++ src/fastcampus/Java369/part3/BruteForce/baekjoon15650.java
@@ -0,0 +1,51 @@
+package fastcampus.Java369.part3.BruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon15650 {
+
+    static int N, M;
+    static int[] selected;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        selected = new int[M + 1];
+
+        dfs(1);
+
+        System.out.println(sb);
+
+    }
+
+
+    public static void dfs(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = selected[k - 1] + 1; i <= N; i++) {
+                selected[k] = i;
+
+                dfs(k + 1);
+
+                selected[k] = 0;
+
+            }
+        }
+
+    }
+
+
+}
