663c45a20156b5ca058f95bd5eef056155da5ffd
데이빗 <appie6069@tw.ac.kr>
2022-07-13 17:27:14 +0900

    추가

---
+++ src/test.java
@@ -0,0 +1,19 @@
+public class test {
+
+    public static void main(String[] args) {
+        String test = "2020-12-01 00:00:00";
+
+
+        System.out.println(test.replaceAll("-", "").substring(0, 8));
+
+
+
+        String ttt= "2020-12-03";
+
+       String test2 = "tb_tran_cardpg_" + ttt.replaceAll("-","").substring(2, 6);
+
+        System.out.println(test2);
+
+    }
+
+}
