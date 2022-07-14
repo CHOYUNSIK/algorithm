bc68c8bb3aa8d25b9e46a03b59f4e8dc082cb027
CHOYUNSIK <appie6069@naver.com>
2022-07-14 11:28:33 +0900

    업로드

--- src/fastcampus/Java369/part3/TwoPointers/baekjoon13144.java
+++ src/fastcampus/Java369/part3/TwoPointers/baekjoon13144.java
@@ -4,7 +4,6 @@ package fastcampus.Java369.part3.TwoPointers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class baekjoon13144 {
@@ -14,21 +13,23 @@ public class baekjoon13144 {
 
         int N = Integer.parseInt(br.readLine());
         int[] a = new int[N + 1];
+        int[] cnt = new int[100001];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 1; i <= N; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
+        long result = 0;
+        for (int L = 1, R = 0; L <= N; L++) {
 
-        int R = 0, result = 0;
-        for (int L = 1; L <= N; L++) {
-            while (R <= N && a[L] == a[++R]) {
-                ++R;
+            while (R + 1 <= N && cnt[a[R + 1]] == 0) {
+                R++;
+                cnt[a[R]]++;
             }
-            if(R - L + 1 > 0){
-                result += R - L + 1;
-            }
+
+            result += R - L + 1;
+            cnt[a[L]]--;
 
         }
 
