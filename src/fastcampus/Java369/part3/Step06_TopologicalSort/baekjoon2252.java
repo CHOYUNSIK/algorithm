97f49da10f9b2f1cec0cf22ae791c4cb20e1b0f8
CHOYUNSIK <appie6069@naver.com>
2022-08-29 11:29:35 +0900

    업로드

---
+++ src/fastcampus/Java369/part3/Step06_TopologicalSort/baekjoon2252.java
@@ -0,0 +1,70 @@
+
+package fastcampus.Java369.part3.Step06_TopologicalSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class baekjoon2252 {
+
+    static int N, M;
+    static ArrayList<Integer>[] graph;
+    static int[] indeg;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        indeg = new int[N + 1];
+
+
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x].add(y);
+            indeg[y]++;
+
+        }
+
+        bfs2252();
+        System.out.println(sb);
+
+    }
+
+    private static void bfs2252() {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++){
+            if (indeg[i] == 0) queue.add(i);
+        }
+
+        while (!queue.isEmpty()){
+            int cur = queue.poll();
+            sb.append(cur).append(" ");
+
+            for (int y : graph[cur]){
+                indeg[y]--;
+                if (indeg[y] == 0) queue.add(y);
+            }
+
+        }
+
+
+    }
+
+}
