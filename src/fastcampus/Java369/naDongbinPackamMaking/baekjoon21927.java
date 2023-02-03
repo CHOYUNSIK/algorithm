47f2cb74a9aa248512f1f69dfe138e75b4acc9f9
CHOYUNSIK <appie6069@naver.com>
2023-02-03 12:40:10 +0900

    업로드

---
+++ src/fastcampus/Java369/naDongbinPackamMaking/baekjoon21927.java
@@ -0,0 +1,65 @@
+package fastcampus.Java369.naDongbinPackamMaking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class baekjoon21927 {
+
+    static int N, M;
+    static ArrayList<Integer>[] lists;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        lists = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            lists[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            lists[y].add(x);
+        }
+
+        int x = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(x);
+
+        int result = 0;
+        boolean[] used = new boolean[N + 1];
+
+        used[x] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            if (cur != x) {
+                result++;
+            }
+            for (int next : lists[cur]) {
+                if (used[next]) continue;
+                used[next] = true;
+                queue.add(next);
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+
+}
---
+++ src/fastcampus/Java369/naDongbinPackamMaking/baekjoon21928.java
@@ -0,0 +1,85 @@
+package fastcampus.Java369.naDongbinPackamMaking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class baekjoon21928 {
+
+    static int N, M;
+
+    static int[][] arr;
+    static boolean[][] visited;
+
+    static int[][] move = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            for (int j = 0; j < M; j++) {
+
+                int r = Integer.parseInt(st.nextToken());
+                int g = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                int sum = (r + g + b) / 3;
+
+                arr[i][j] = sum;
+            }
+
+        }
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] >= T) {
+                    arr[i][j] = 255;
+                } else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (visited[i][j] || arr[i][j] != 255) continue;
+                result++;
+                dfs(i, j);
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < move.length; i++) {
+            int nx = x + move[i][0];
+            int ny = y + move[i][1];
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (visited[nx][ny] || arr[nx][ny] != 255) continue;
+
+            dfs(nx, ny);
+        }
+
+    }
+
+
+}
