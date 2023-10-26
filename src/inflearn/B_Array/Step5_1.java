28d2472184b8d3835b495175b2bf3ad4be4632c8
CHOYUNSIK <appie6069@naver.com>
2023-10-26 12:59:13 +0900

    업로드

--- src/inflearn/B_Array/Step5_1.java
+++ src/inflearn/B_Array/Step5_1.java
@@ -6,8 +6,24 @@ import java.io.InputStreamReader;
 
 public class Step5_1 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int i = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1];
+
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) result++;
+
+            for (int j = i; j <= n; j = j + i) {
+                arr[j] = 1;
+            }
+
+        }
+
+        System.out.println(result);
     }
 }
+
---
+++ src/inflearn/B_Array/Step6_1.java
@@ -0,0 +1,39 @@
+package inflearn.B_Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Step6_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+            if (IsMinority(num)) {
+                System.out.print(num + " ");
+            }
+            ;
+
+        }
+    }
+
+    private static boolean IsMinority(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
