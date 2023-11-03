84262e18ca5b62f1313e31e8059294e5167054a2
CHOYUNSIK <appie6069@naver.com>
2023-11-03 11:20:33 +0900

    업로드

---
+++ src/inflearn/B_Array/Step11_1.java
@@ -0,0 +1,46 @@
+package inflearn.B_Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Step11_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][5];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+
+            if (cnt > max) {
+                max = cnt;
+                result = i + 1;
+            }
+        }
+
+
+        System.out.println(result);
+
+
+    }
+}
